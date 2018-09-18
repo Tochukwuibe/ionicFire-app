8f7877e8266e85830f629acb90d17fd968fba027
Tochukwuibe <tochukwu.claver@gmail.com>
2018-09-18 16:54:59 -0400

    Update environment.ts

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,11 +1,6 @@
 export const environment = {
     production: false,
     firebase : {
-    apiKey: "AIzaSyC-NlmXioAI44C3F2_Ko7dW-UXiAzkk2i8",
-    authDomain: "chatapptoch.firebaseapp.com",
-    databaseURL: "https://chatapptoch.firebaseio.com",
-    projectId: "chatapptoch",
-    storageBucket: "chatapptoch.appspot.com",
-    messagingSenderId: "881955339727"
+    
     }
 }
